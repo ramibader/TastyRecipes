220fe29f996b4a5de03856a68a487330e9b18157
Dominik Grodl <dominikgrodl@icloud.com>
2021-09-12 21:34:49 +0200

    Added description to Wrapper

--- Tasty Recipes/Models/Wrapper.swift
+++ Tasty Recipes/Models/Wrapper.swift
@@ -5,13 +5,17 @@
 //  Created by Dominik Grodl on 12.09.2021.
 //
 
+///This is a NSObject to Struct wrapper. It encapsulates custom structs to NSObject, which is needed to save to NSCache, which does not support Swift structs. Wrapper takes a parameter and saves it into a value, making the stored struct available via Wrapper.value
+///
+///*NSCacheInstance*.setObject(Wrapper(*value*), forKey: *cacheKey*)
+
 import Foundation
 
 class Wrapper<S>: NSObject {
 
 	let value: S
 
-	init(_ _struct: S) {
-		self.value = _struct
+	init(_ value: S) {
+		self.value = value
 	}
 }
